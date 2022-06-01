77721c527ea397862092a3e7ad7940d4225170e9
Kenny Chan <hoiyat0210@gmail.com>
2022-06-01 05:00:40 -0400

    stack expanded removed

--- lib/components/aboutAssets.dart
+++ lib/components/aboutAssets.dart
@@ -15,15 +15,40 @@ class AboutAssets extends StatelessWidget {
           ? const BoxConstraints(maxWidth: 400, maxHeight: 500)
           : const BoxConstraints(maxWidth: 500, maxHeight: 525),
       child: Align(
-          alignment: Alignment.topRight,
-          child: Flexible(
-            child: Stack(
-              clipBehavior: Clip.none,
-              children: [
-                ConstrainedBox(
+        alignment: Alignment.topRight,
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            ConstrainedBox(
+              constraints: shouldShrinkAssets
+                  ? const BoxConstraints(maxHeight: 275)
+                  : const BoxConstraints(maxHeight: 325),
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(32),
+                  child: GlassMorphism(
+                    opacity: 0.4,
+                    constraints: shouldShrinkAssets
+                        ? const BoxConstraints(maxHeight: 275)
+                        : const BoxConstraints(maxHeight: 325),
+                    color: scheme.secondaryContainer,
+                    blur: 10,
+                    child: Image.asset(
+                      './assets/memoji_1.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+                right: shouldShrinkAssets ? 75 : 150,
+                top: shouldShrinkAssets ? 225 : 250,
+                child: ConstrainedBox(
                   constraints: shouldShrinkAssets
-                      ? const BoxConstraints(maxHeight: 275)
-                      : const BoxConstraints(maxHeight: 325),
+                      ? const BoxConstraints(maxHeight: 325)
+                      : const BoxConstraints(maxHeight: 375),
                   child: AspectRatio(
                     aspectRatio: 1,
                     child: ClipRRect(
@@ -31,47 +56,21 @@ class AboutAssets extends StatelessWidget {
                       child: GlassMorphism(
                         opacity: 0.4,
                         constraints: shouldShrinkAssets
-                            ? const BoxConstraints(maxHeight: 275)
-                            : const BoxConstraints(maxHeight: 325),
+                            ? const BoxConstraints(maxHeight: 325)
+                            : const BoxConstraints(maxHeight: 375),
                         color: scheme.secondaryContainer,
                         blur: 10,
                         child: Image.asset(
-                          './assets/memoji_1.png',
+                          './assets/memoji_2.png',
                           fit: BoxFit.cover,
                         ),
                       ),
                     ),
                   ),
-                ),
-                Positioned(
-                    right: shouldShrinkAssets ? 75 : 150,
-                    top: shouldShrinkAssets ? 225 : 250,
-                    child: ConstrainedBox(
-                      constraints: shouldShrinkAssets
-                          ? const BoxConstraints(maxHeight: 325)
-                          : const BoxConstraints(maxHeight: 375),
-                      child: AspectRatio(
-                        aspectRatio: 1,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(32),
-                          child: GlassMorphism(
-                            opacity: 0.4,
-                            constraints: shouldShrinkAssets
-                                ? const BoxConstraints(maxHeight: 325)
-                                : const BoxConstraints(maxHeight: 375),
-                            color: scheme.secondaryContainer,
-                            blur: 10,
-                            child: Image.asset(
-                              './assets/memoji_2.png',
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                      ),
-                    )),
-              ],
-            ),
-          )),
+                )),
+          ],
+        ),
+      ),
     );
   }
 }
